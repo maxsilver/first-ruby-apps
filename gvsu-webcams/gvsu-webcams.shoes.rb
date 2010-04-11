be813bb6a6263376ecf50ea088790ab43d6b5cdb
Matthew Seeley <matthew@threadlight.com>
2010-04-11 00:27:35 -0400

    Bugfix for timer image updating.

--- gvsu-webcams/gvsu-webcams.shoes.rb
+++ gvsu-webcams/gvsu-webcams.shoes.rb
@@ -2,7 +2,6 @@ Shoes.app :title => "GVSU Webcams", :width => 900, :height => 547, :resizable =>
 	background white
 	background "headerBkg.jpg", :height => 67
 
-
 	@header = rect 0,0,200,67
 	@header.style(:fill => "gvsu-logo.gif", :strokewidth => 0)	
 	
@@ -18,45 +17,18 @@ Shoes.app :title => "GVSU Webcams", :width => 900, :height => 547, :resizable =>
 		stack :height => 40, :width => 260 do
 			background red
 		end
-		stack :height => 40, :width => 260 do
-			background blue
-		end		
-		stack :height => 40, :width => 260 do
-			background red
-		end
-		stack :height => 80, :width => 260 do
-			background blue
-		end		
-		stack :height => 80, :width => 260 do
-			background red
-		end
-		stack :height => 40, :width => 260 do
-			background blue
-		end		
-		stack :height => 40, :width => 260 do
-			background red
-		end
-		stack :height => 40, :width => 260 do
-			background blue
-		end		
-		stack :height => 80, :width => 260 do
-			background red
-		end
-		stack :height => 80, :width => 260 do
-			background blue
-		end		
 												
 	end
 	
 	stack :left => 260, :top => 67, :width => 640, :height => 480 do
 		background black
-		imgpath =  "http://148.61.171.201//axis-cgi/jpg/image.cgi?resolution=640x480"
-		@viewer = image imgpath
+		@imgpath =  "http://148.61.165.230//axis-cgi/jpg/image.cgi?resolution=640x480"
+		@viewer = image @imgpath
 	end
 
-	
-	every 1 do
-    	@viewer.path = imgpath + "&time=" + Time.Now.to_f
-     	@msg.replace("&time=" + Time.Now.to_f)
+
+	every 5 do
+    	@viewer.path = (@imgpath + "&time=" +  Time.now.strftime("%I%M%S%m%d%Y"))
+     	@msg.replace(Time.now.strftime("%I%M%S%m%d%Y"))	    	
    	end
 end
