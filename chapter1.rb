6dd9319ce08d621cc6ec5dfba8f7d6b126251128
Matthew Seeley <matthew@threadlight.com>
2010-04-10 22:07:18 -0400

    Added a simple class, with some properties.

--- chapter1.rb
+++ chapter1.rb
@@ -3,6 +3,26 @@
 
 
 
+# let's make a class
+class Person
+	attr_accessor :name,
+				  :age,
+				  :gender,
+				  :hometown
+end
+
+# and lets do something with that class.
+FunnyMan = Person.new
+FunnyMan.name     = "Jon Stewart"
+FunnyMan.age      = 47 # as of 4/10/2010
+FunnyMan.gender   = "male"
+FunnyMan.hometown = "New York, NY"
+
+
+print "Our next guest is ", FunnyMan.name, 
+	   " from beautiful ", FunnyMan.hometown,
+	   "\n"
+
 # a simple loop
 10.times do print "Hello, World!" + "\n" end
 
