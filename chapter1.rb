d6f96d12eac4970581f1f34a04ef84d4cc45fff0
Matthew Seeley <matthew@threadlight.com>
2010-04-10 22:44:36 -0400

    Multiline string literals!

--- chapter1.rb
+++ chapter1.rb
@@ -2,6 +2,23 @@
 # from Chapter 1 - Beginning Ruby
 
 
+# A different way to make string literals
+X = %q{
+	We hold these truths to be self-evident, 
+	that all men are created equal, 
+	that they are endowed by their Creator 
+	with certain unalienable Rights,
+	that among these are Life, Liberty and the pursuit of Happiness. 
+	That to secure these rights, 
+	Governments are instituted among Men, 
+	deriving their just powers from the consent of the governed, 
+	That whenever any Form of Government becomes destructive of these ends, 
+	it is the Right of the People to alter or to abolish it, 
+	and to institute new Government, 
+	laying its foundation on such principles and organizing its powers in such form, 
+	as to them shall seem most likely to effect their Safety and Happiness. 
+}
+
 #Constants are capatilized
 PI = 3.14159265428 #never change that!
 my_age = 21 # this changes
