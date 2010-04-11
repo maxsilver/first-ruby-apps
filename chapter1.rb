efa84408be9f2fa225b72d878e6f627421cf49f5
Matthew Seeley <matthew@threadlight.com>
2010-04-10 22:35:58 -0400

    Simple conditionals. Built in string methods.

--- chapter1.rb
+++ chapter1.rb
@@ -2,8 +2,23 @@
 # from Chapter 1 - Beginning Ruby
 
 
+#simple conditionals
+age = 9000
+puts "You're not born?"    if age < 0
+puts "You're a baby!"      if age >= 0 && age < 5
+puts "You're a child!"     if age >= 5 && age < 13
+puts "You're a teenager!"  if age >= 13 && age < 19
+puts "You're an adult!"    if age >= 20 && age < 150
+puts "You're likely dead!" if age >= 150 && age < 9000
+puts "You're a mummy!"     if age >= 9000
 
-
+# there are lots of useful methods built into default objects ('datatypes')
+verse = "We hold these truths to be self-evident, that all men are created equal..."
+puts verse, "\n"
+puts verse.length, "\n"
+puts verse.upcase, "\n"
+puts verse.reverse, "\n"
+puts verse.swapcase.reverse.sum, "\n"
 
 # turns out, put is a method of a class (a superclass). Ruby's object is called 'Kernel'
 Kernel.puts "the method 'puts' is from class type : ", Kernel.class
@@ -38,7 +53,7 @@ lassie.name = "Lassie"
 lassie.age = 20
 
 # lassie barks a lot
-10.times do
+3.times do
 	lassie.bark
 end
 
@@ -70,7 +85,7 @@ print "Our next guest is ", FunnyMan.name,
 	   "\n"
 
 # a simple loop
-10.times do print "Hello, World!" + "\n" end
+3.times do print "Hello, World!" + "\n" end
 
 # commas can concatinate print requests, similar to PHP echo("string", "string")
 print "Hello. Did you know that 2 + 2 = ", (2 + 2)
