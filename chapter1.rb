09e68a131dcf485860ac2f1a86ff7d462ebe8278
Matthew Seeley <matthew@threadlight.com>
2010-04-10 21:56:07 -0400

    Simple loops, print statements

---
+++ chapter1.rb
@@ -0,0 +1,10 @@
+# Random Ruby Stuff
+# from Chapter 1 - Beginning Ruby
+
+
+
+# a simple loop
+10.times do print "Hello, World!" + "\n" end
+
+# commas can concatinate print requests, similar to PHP echo("string", "string")
+print "Hello. Did you know that 2 + 2 = ", (2 + 2)
